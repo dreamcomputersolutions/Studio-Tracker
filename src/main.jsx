275042015afdbed7e5c047172659cc86605e3cd9
Dinesh <dinesh@example.com>
2025-11-25 11:18:43 +0530

    Restore CSS bridge in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // <--- THIS IS THE MISSING MAGIC LINE
+import './index.css'  // <--- CHECK THIS LINE. It connects the styling.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
